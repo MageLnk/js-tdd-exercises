1d83fd3a74d54e76838eed64433772cb9b4c502a
MageLink <mage.link.zhezard@gmail.com>
2023-03-07 17:42:30 -0300

    Backup

--- I.pass-tests/01-add-one/add-one.js
+++ I.pass-tests/01-add-one/add-one.js
@@ -1,1 +1,18 @@
-module.exports = function(numbers) {};
+const addOne = (numbers) => {
+  if (Array.isArray(numbers) === false) {
+    throw new Error("Invalid format");
+  }
+  const SearchForString = numbers.map((result) => typeof result === "string");
+  if (SearchForString.includes(true)) throw new Error("Invalid format");
+
+  //const checkValidNumber = numbers.filter((result) => isNaN(result));
+  //if (checkValidNumber.lenght === 0) {
+  //  return [];
+  //}
+
+  return numbers.map((result) => result + 1);
+};
+
+module.exports = {
+  addOne,
+};
--- I.pass-tests/01-add-one/add-one.test.js
+++ I.pass-tests/01-add-one/add-one.test.js
@@ -1,12 +1,70 @@
-var addOne = require("./add-one.js");
+const { addOne } = require("./add-one.js");
 
-test("Add 1 to each item in myArray", function() {
-  var myArray = [31, 57, 12, 5];
+// My Version
+describe("Given the addOne function", () => {
+  it("Will add one to [31, 57, 12, 5] returning [32, 58, 13, 6] and keeping the original array", () => {
+    //Arrange
+    const myArray = [31, 57, 12, 5];
+    const unchanged = [31, 57, 12, 5];
+    const expected = [32, 58, 13, 6];
 
-  var unchanged = [31, 57, 12, 5];
-  var expected = [32, 58, 13, 6];
-  var output = addOne(myArray);
+    //Act
+    const output = addOne(myArray);
 
-  expect(output).toEqual(expected);
-  expect(myArray).toEqual(unchanged);
+    //Assert
+    expect(output).toEqual(expected);
+    expect(myArray).toEqual(unchanged);
+  });
+
+  it("Will add one to [] returning []", () => {
+    //Arrange
+    const myArray = [];
+    const expected = [];
+
+    //Act
+    const output = addOne(myArray);
+
+    //Assert
+    expect(output).toEqual(expected);
+  });
+  it("Will add one to entire negative array [-31, -57, -12, -5] to [-30, -56, -13, -4]", () => {
+    // Arrange
+    const myArray = [-31, -57, -12, -5];
+    const unchanged = [-31, -57, -12, -5];
+    const expected = [-30, -56, -11, -4];
+    // Act
+    const output = addOne(myArray);
+    // Assert
+    expect(output).toEqual(expected);
+    expect(myArray).toEqual(unchanged);
+  });
+
+  it("Will throw error when received null", () => {
+    // Arrange
+    const myArray = null;
+    // Act & Assert
+    expect(() => addOne(myArray)).toThrow(/^Invalid format$/);
+  });
+
+  it("Will throw error when received boolean", () => {
+    // Arrange
+    const myArray = false;
+    // Act and Assert
+    expect(() => addOne(myArray)).toThrow(/^Invalid format$/);
+  });
+
+  it("Will throw error when received a string", () => {
+    // Arrange
+    const myArray = "test";
+    // Act and Assert
+    expect(() => addOne(myArray)).toThrow(/^Invalid format$/);
+  });
+
+  it("Will return same array if recieved an array with Strings", () => {
+    // Arrange
+    const myArray = [55, "-57", 53, 14];
+    const expected = [55, "-57", 53, 14];
+    // Act and Assert
+    expect(() => addOne(myArray)).toThrow(/^Invalid format$/);
+  });
 });
