1602e84d967e71c689794f23a7ec2a2542954a26
MageLink <mage.link.zhezard@gmail.com>
2023-03-15 18:54:39 -0300

    Minnor changes

--- III.tdd-katas/calculator-tdd/add.js
+++ III.tdd-katas/calculator-tdd/add.js
@@ -1,20 +1,17 @@
 const add = (word) => {
   if (!word) return 0;
 
-  const numbers = word.match(/-?\d+/g) || [];
-  // Expresión regular para obtener los números
+  const numbers = word.match(/-?\d+/g);
+  // Expresión regular para obtener los números y separarlos de los caracteres
 
-  const transformToNumers = numbers.map((num) => +num);
-  //const parsedNumbers = numbers.map((num) => parseInt(num, 10)); // Será mejor parseInt?
+  const parsedNumbers = numbers.map((num) => parseInt(num, 10));
 
-  const negativeNumbers = transformToNumers.filter((num) => num < 0);
+  const negativeNumbers = parsedNumbers.filter((num) => num < 0);
   if (negativeNumbers.length > 0) throw new Error(`Negatives not allowed: ${negativeNumbers.join(", ")}`);
 
-  const ignoringBiggerNumbers = transformToNumers.filter((num) => num < 1001);
+  const ignoringBiggerNumbers = parsedNumbers.filter((num) => num < 1001);
 
-  const sumNumbers = ignoringBiggerNumbers.reduce((partialSum, newValue) => partialSum + newValue, 0);
-
-  return sumNumbers;
+  return ignoringBiggerNumbers.reduce((partialSum, newValue) => partialSum + newValue, 0);
 };
 
 module.exports = {
