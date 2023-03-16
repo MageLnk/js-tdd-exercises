ae9f20a26192a7f49ceff91736e74d26000b6578
MageLink <mage.link.zhezard@gmail.com>
2023-03-16 11:01:05 -0300

    Backup for fizz-buzz

---
+++ III.tdd-katas/fizz-buzz/fizz-buzz.js
@@ -0,0 +1,16 @@
+const fizzbuzz = (num) => {
+  //const arrayBasedOnNum = Array.from(Array(num).keys(), (x) => x + 1); // Versión ES6
+  // const arrayBasedOnNum = [...Array(10).keys(), x => x + 1] // Versión usando spread operator
+  const arrayBasedOnNum = Array.from({ length: num }, (_, i) => i + 1); // Versión manipulable, sin hace otro map
+
+  const result = arrayBasedOnNum.map((num) => {
+    if (num % 5 === 0 && num % 3 === 0) return "FizzBuzz";
+    if (num % 5 === 0) return "Buzz";
+    if (num % 3 === 0) return "Fizz";
+    return num;
+  });
+
+  return result.join(", ");
+};
+
+module.exports = fizzbuzz;
--- III.tdd-katas/fizz-buzz/fizz-buzz.test.js
+++ III.tdd-katas/fizz-buzz/fizz-buzz.test.js
@@ -1,11 +1,63 @@
-var fizzbuzz = require("./fizz-buzz");
+const fizzbuzz = require("./fizz-buzz");
 
-test("prints 1", function() {
-  var result = fizzbuzz(1);
-  expect(result).toEqual("1");
-});
+describe("Given the fizzbuzz function", () => {
+  test("It should prints '1' when the number is 1", () => {
+    // Arrange
+    const number = 1;
+    const expected = "1";
 
-test("prints 1,2", function() {
-  var result = fizzbuzz(2);
-  expect(result).toEqual("1, 2");
+    // Act
+    const output = fizzbuzz(number);
+
+    // Assert
+    expect(output).toEqual(expected);
+  });
+
+  test("It should prints '1, 2' when the number is 2", () => {
+    // Arrange
+    const number = 2;
+    const expected = "1, 2";
+
+    // Act
+    const output = fizzbuzz(number);
+
+    // Assert
+    expect(output).toEqual(expected);
+  });
+
+  test("It should prints '1, 2, Fizz' when the number is 3", () => {
+    // Arrange
+    const number = 3;
+    const expected = "1, 2, Fizz";
+
+    // Act
+    const output = fizzbuzz(number);
+
+    // Assert
+    expect(output).toEqual(expected);
+  });
+
+  test("It should prints '1, 2, Fizz, 4, Buzz' when the number is 5", () => {
+    // Arrange
+    const number = 5;
+    const expected = "1, 2, Fizz, 4, Buzz";
+
+    // Act
+    const output = fizzbuzz(number);
+
+    // Assert
+    expect(output).toEqual(expected);
+  });
+
+  test("It should prints '1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14, FizzBuzz' when the number is 15", () => {
+    // Arrange
+    const number = 15;
+    const expected = "1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14, FizzBuzz";
+
+    // Act
+    const output = fizzbuzz(number);
+
+    // Assert
+    expect(output).toEqual(expected);
+  });
 });
